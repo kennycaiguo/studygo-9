f916613104f23cf20a4427dc6f217d66076da975
wangLei <wangleivic@126.com>
2019-05-09 18:29:01 +0800

    新增一些将要用到的变量

--- pool/main.go
+++ pool/main.go
@@ -9,8 +9,56 @@ import (
 	"time"
 )
 
+var maxWorkers = 20
+
 // MyService 业务接口
 type MyService struct {
+}
+
+// Job 表示要执行的作业
+type Job struct {
+	MyService MyService
+}
+
+// JobQueue 作业队列
+var JobQueue chan Job
+
+// Worker 执行作业的工人
+type Worker struct {
+	WorkerPool chan chan Job
+	JobChannel chan Job
+}
+
+// NewWorker 新建一个工人
+func NewWorker(workerPool chan chan Job) Worker {
+	return Worker{
+		WorkerPool: workerPool,
+		JobChannel: make(chan Job),
+	}
+}
+
+// Start 监听
+func (w Worker) Start() {
+	go func() {
+		for {
+			w.WorkerPool <- w.JobChannel
+			select {
+			case job := <-w.JobChannel:
+				//有工作任务时，开始执行业务接口的方法
+				job.MyService.writeInfo()
+			}
+		}
+	}()
+}
+
+func init() {
+	// 创建工人池
+	//pool := make(chan chan Job, maxWorkers)
+	// 创建一定数量的工人
+	for i := 0; i < maxWorkers; i++ {
+
+	}
+
 }
 
 func main() {
